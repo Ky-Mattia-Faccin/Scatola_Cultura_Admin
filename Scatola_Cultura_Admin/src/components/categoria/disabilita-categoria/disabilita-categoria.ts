d51675092ae9aaa63937e57d88096e3062193702
MCCR-19 <cariolato.michael@gmail.com>
2025-06-17 14:13:24 +0200

    componenti categorie

--- Scatola_Cultura_Admin/src/components/categoria/disabilita-categoria/disabilita-categoria.ts
+++ Scatola_Cultura_Admin/src/components/categoria/disabilita-categoria/disabilita-categoria.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface catDisabilita {
+  categoria: string;
+  descrizione: string;
+  checked: boolean;
+}
 
 @Component({
   selector: 'app-disabilita-categoria',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './disabilita-categoria.html',
-  styleUrl: './disabilita-categoria.css'
+  styleUrl: './disabilita-categoria.css',
 })
-export class DisabilitaCategoria {
+export class DisabilitaCategoria  implements OnInit{
+  
+  disabilita!: catDisabilita[];
 
+  disableCategories() {
+    this.disabilita = this.disabilita.filter((cat) => !cat.checked);
+  }
+
+
+  ngOnInit(): void {
+    this.disabilita=disabilitaMock
+  }
 }
+
+
+export const disabilitaMock: catDisabilita[] = [
+  {
+    categoria: 'Manutenzione',
+    descrizione: 'Chiuso per lavori fino a fine mese',
+    checked: false
+  },
+  {
+    categoria: 'Aggiornamento',
+    descrizione: 'Aggiornamento software in corso',
+    checked: false
+  },
+  {
+    categoria: 'Pulizia',
+    descrizione: 'Pulizie programmate settimanali',
+    checked: false
+  },
+  {
+    categoria: 'Eventi Speciali',
+    descrizione: 'Prenotazioni bloccate per evento privato',
+    checked: false
+  },
+  {
+    categoria: 'Emergenza',
+    descrizione: 'Inagibilità temporanea per emergenza',
+    checked: false
+  }
+];
